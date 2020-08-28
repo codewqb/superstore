c15e8a810ca4063803d52d6de6f05816ac90bde7
codewqb <1219223545@qq.com>
2020-08-28 19:14:47 +0800

    完成

--- src/common/tools.js
+++ src/common/tools.js
@@ -1,27 +1,27 @@
 //  节流函数
- export function throttle(fn, delay) {
-   let flag = true;
-   return function (...args) {
-     if (!flag) {
-       return;
-     } 
-       flag = false;
-       setTimeout(() => {
-         fn.apply(this, args);
-         flag = true;
-       }, delay);
-     
-   }
- }
- // 防抖函数
- export function debounce(fn, delay) {
-   let timer = null;
-   return function (...args) {
-     if (timer) {
-       clearTimeout(timer);
-     }
-     timer = setTimeout(() => {
-       fn.apply(this, args);
-     }, delay);
-   }
- }
+export function throttle(fn, delay) {
+  let flag = true;
+  return function (...args) {
+    if (!flag) {
+      return;
+    }
+    flag = false;
+    setTimeout(() => {
+      fn.apply(this, args);
+      flag = true;
+    }, delay);
+
+  }
+}
+// 防抖函数
+export function debounce(fn, delay) {
+  let timer = null;
+  return function (...args) {
+    if (timer) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(() => {
+      fn.apply(this, args);
+    }, delay);
+  }
+}
